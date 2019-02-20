80010c028333d7c172ce99e3e5093ba6726819a0
Matthew Shin <mshin1995@gmail.com>
2019-02-20 01:45:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |x,y|
+    y <=> x 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |x|
+    x.length 
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.map do |string|
+    if array[1] == string
+      string
+    else string << "s"
+    end
+  end
+end
